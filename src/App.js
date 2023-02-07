b57b5fbd3ee6cb4df597517e337bb9dce02af5de
Nicoov <nicolas_ignacio_1@hotmail.com>
2023-02-07 17:04:20 -0300

    update 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import preguntas from './preguntas';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+  const [preguntasActual, setPreguntasActual] = useState(0);
+  const [puntuacion, setPuntuacion] = useState(0);
+  const [finished, setFinished] = useState(false);
+
+
+  const handleAnswerSubmit = (isCorrect, e) => {
+    if (isCorrect) {
+      setPuntuacion(puntuacion + 1);
+    }
+    e.target.classList.add(isCorrect ? "correct" : "incorrect")
+
+    if (preguntasActual === preguntas.length - 1) {
+      setFinished(true);
+    } else {
+      setPreguntasActual(preguntasActual + 1)
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='left-side'>
+        <div className='answer-number'>
+          <span>Pregunta {preguntasActual + 1} de </span> {preguntas.length}
+        </div>
+        <div className='answer-title'>{preguntas[preguntasActual].titulo}
+        </div>
+      </div>
+      <div className='rigth-side'>
+        {preguntas[preguntasActual].opciones.map((respuesta) => (
+          <button key={respuesta.Respuesta} onClick={(e) => { handleAnswerSubmit(respuesta.isCorrect, e) }}>{respuesta.Respuesta}</button>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/preguntas.js
@@ -0,0 +1,24 @@
+const preguntas = [
+    {
+        titulo : "Cuales son los cinco tipos de sabores primarios",
+        opciones: [
+            {Respuesta: "dulce, amargom acido, salado", isCorrect: true},
+            {Respuesta: "dulce, verde, acido, salado", isCorrect: false},
+            {Respuesta: "dulce, amargom amarillo, salado", isCorrect: false},
+            {Respuesta: "dulce, amargom acido, rojo", isCorrect: false},
+        ],
+    },
+
+    {
+        titulo : "Cual es el lugar mas frio de la tierra",
+        opciones: [
+            {Respuesta: "Polo sur", isCorrect: false},
+            {Respuesta: "Antartida", isCorrect: true},
+            {Respuesta: "Santiago", isCorrect: false},
+            {Respuesta: "Polo norte", isCorrect: false},
+        ],
+    },
+
+]
+
+export default preguntas;
