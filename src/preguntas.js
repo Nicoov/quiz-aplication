1965831a745faffa5358c80f2d821b5b295c5fb5
Nicoov <nicolas_ignacio_1@hotmail.com>
2023-02-15 14:13:39 -0300

    update: new answers

--- src/preguntas.js
+++ src/preguntas.js
@@ -1,23 +1,105 @@
 const preguntas = [
     {
-        titulo : "Cuales son los cinco tipos de sabores primarios",
+        titulo: "¿Cuales son los cinco tipos de sabores primarios?",
         opciones: [
-            {Respuesta: "dulce, amargom acido, salado", isCorrect: true},
-            {Respuesta: "dulce, verde, acido, salado", isCorrect: false},
-            {Respuesta: "dulce, amargom amarillo, salado", isCorrect: false},
-            {Respuesta: "dulce, amargom acido, rojo", isCorrect: false},
+            { Respuesta: "Dulce, amargo acido, salado", isCorrect: true },
+            { Respuesta: "Dulce, verde, acido, salado", isCorrect: false },
+            { Respuesta: "Dulce, amargo amarillo, salado", isCorrect: false },
+            { Respuesta: "Dulce, amargo acido, rojo", isCorrect: false },
         ],
     },
 
     {
-        titulo : "Cual es el lugar mas frio de la tierra",
+        titulo: "¿Cual es el lugar mas frio de la tierra?",
         opciones: [
-            {Respuesta: "Polo sur", isCorrect: false},
-            {Respuesta: "Antartida", isCorrect: true},
-            {Respuesta: "Santiago", isCorrect: false},
-            {Respuesta: "Polo norte", isCorrect: false},
+            { Respuesta: "Polo sur", isCorrect: false },
+            { Respuesta: "Antartida", isCorrect: true },
+            { Respuesta: "Santiago", isCorrect: false },
+            { Respuesta: "Polo norte", isCorrect: false },
         ],
     },
+
+    {
+        titulo: "¿Cómo se llama la capital de Mongolia?",
+        opciones: [
+            { Respuesta: "Ciudad del cabo", isCorrect: false },
+            { Respuesta: "Santiago", isCorrect: false },
+            { Respuesta: "Ulan Bator", isCorrect: true },
+            { Respuesta: "Bloemfontein", isCorrect: false },
+        ],
+    },
+
+    {
+        titulo: "¿¿Cuál es el río más largo del mundo??",
+        opciones: [
+            { Respuesta: "Rio nilo", isCorrect: false },
+            { Respuesta: "Amazonas", isCorrect: true },
+            { Respuesta: "Rio misisipi", isCorrect: false },
+            { Respuesta: "Mekong", isCorrect: false },
+        ],
+    },
+
+    {
+        titulo: "¿Cuantos huesos tiene el ser humano?",
+        opciones: [
+            { Respuesta: "210", isCorrect: false },
+            { Respuesta: "190", isCorrect: false },
+            { Respuesta: "206", isCorrect: true },
+            { Respuesta: "220", isCorrect: false },
+        ],
+    },
+
+    {
+        titulo: "¿Cuando termino la segunda guerra mundial?",
+        opciones: [
+            { Respuesta: "1941", isCorrect: false },
+            { Respuesta: "1950", isCorrect: false },
+            { Respuesta: "1944", isCorrect: false },
+            { Respuesta: "1945", isCorrect: true },
+        ],
+    },
+    {
+        titulo: "¿Qué año llegó Cristóbal Colón a América?",
+        opciones: [
+            { Respuesta: "1300", isCorrect: false },
+            { Respuesta: "1492", isCorrect: true },
+            { Respuesta: "1568", isCorrect: false },
+            { Respuesta: "1669", isCorrect: false },
+        ],
+    },
+
+    {
+        titulo: "¿Qué país tiene forma de bota?",
+        opciones: [
+            { Respuesta: "Alemania", isCorrect: false },
+            { Respuesta: "Italia", isCorrect: true },
+            { Respuesta: "Chile", isCorrect: false },
+            { Respuesta: "Sudafrica", isCorrect: false },
+        ],
+    },
+    {
+        titulo: "¿En qué lugar del cuerpo se produce la insulina?",
+        opciones: [
+            { Respuesta: "Pancreas", isCorrect: false },
+            { Respuesta: "Estomago", isCorrect: false },
+            { Respuesta: "Riñones", isCorrect: false },
+            { Respuesta: "Pancreas", isCorrect: true },
+        ],
+    },
+
+    {
+        titulo: "¿Cual es el color que representa la esperanza?",
+        opciones: [
+            { Respuesta: "Blanco", isCorrect: false },
+            { Respuesta: "Verde", isCorrect: true },
+            { Respuesta: "Rosa", isCorrect: false },
+            { Respuesta: "Amarillo", isCorrect: false },
+        ],
+    },
+
+
+
+
 
 ]
 
